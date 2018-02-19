261ee7ec1ddbbfaf3c0dd09c1eb215a6b0b3fa37
karyotype96 <seancurryoakmont@gmail.com>
2018-02-19 16:23:42 -0500

    Update #7

--- Sorts.h
+++ Sorts.h
@@ -138,20 +138,6 @@ class Sorts
                 a[k++] = A2[j++];
         }
 
-        static int part(int *a, int low, int high){
-            int pivot = a[high];
-
-            int i = (low - 1);
-            for (int j = low; j <= high-1; j++){
-                if (a[j] <= pivot){
-                    i++;
-                    swap(a[i], a[j]);
-                }
-            }
-            swap(a[i+1], a[high]);
-            return (i + 1);
-        }
-
         static void print_array(int *a, int size){
             std::cout << "Array: ";
             for (int i = 0; i < size; i++){
